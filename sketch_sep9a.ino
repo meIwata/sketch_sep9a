9b49ff6100b8bd80f5cec532107bab652f271c13
meIwata <waleilulu@gmail.com>
2025-09-09 11:53:12 +0800

    Switch LED blink to serial output in loop
    Commented out LED blinking code and added serial communication to print 'Hello computer' every 500ms. Serial port is initialized at 115200 baud in setup.

--- sketch_sep9a.ino
+++ sketch_sep9a.ino
@@ -1,16 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200); // 設定串口鮑率
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // delay(1000);
+  // digitalWrite(LED_BUILTIN, LOW);
+  // delay(500);
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // delay(500);
+  // digitalWrite(LED_BUILTIN, LOW);
+  // delay(1000);
+
+  Serial.println("Hello computer"); // 要發送的文字
   delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
 }
